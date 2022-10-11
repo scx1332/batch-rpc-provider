3f015d3ced2d5ea99795adcd96a931d5ca2ce97f
scx1332 <sieciech@gmail.com>
2022-10-11 18:48:24 +0200

    Update tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,3 +1,4 @@
+import logging
 import unittest
 from batch_rpc_provider import BatchRpcProvider, BatchRpcException
 
@@ -5,6 +6,8 @@ from batch_rpc_provider import BatchRpcProvider, BatchRpcException
 class TestSimple(unittest.IsolatedAsyncioTestCase):
     async def test_error(self):
         try:
+            # errors are expected so disable them to avoid confusion
+            logging.getLogger("batch_rpc_provider.batch_rpc_provider").setLevel(logging.CRITICAL)
             p = BatchRpcProvider('http://dummy:8545', 10000)
             await p.get_chain_id()
         except BatchRpcException as ex:
@@ -13,11 +16,14 @@ class TestSimple(unittest.IsolatedAsyncioTestCase):
             self.fail("Unexpected exception: {}".format(ex))
 
     async def test_polygon_chain_id(self):
-        try:
-            p = BatchRpcProvider('https://polygon-rpc.com', 10000)
-            self.assertEqual(await p.get_chain_id(), 137)
-        except Exception as ex:
-            self.fail("Unexpected exception: {}".format(ex))
+        p = BatchRpcProvider('https://polygon-rpc.com', 10000)
+        chain_id = await p.get_chain_id()
+        self.assertEqual(chain_id, 137)
+
+    async def test_polygon_blocknum(self):
+        p = BatchRpcProvider('https://polygon-rpc.com', 10000)
+        block_num = await p.get_latest_block()
+        self.assertTrue(block_num > 30000000)
 
 
 if __name__ == '__main__':
